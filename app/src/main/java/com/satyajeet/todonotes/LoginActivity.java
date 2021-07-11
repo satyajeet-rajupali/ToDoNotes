acbc5a8eb6d9ea968493878cb8578358513707f7
satyajeet <satyajeetrajupali@gmail.com>
2021-07-11 20:36:11 +0530

    Initial commit

---
+++ app/src/main/java/com/satyajeet/todonotes/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.satyajeet.todonotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextFullName, editTextUserName;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // To get hold of the widgets described in LoginActivity.
+        editTextFullName = findViewById(R.id.editTextFullName);
+        editTextUserName = findViewById(R.id.editTextUserName);
+        loginButton = findViewById(R.id.loginButton);
+
+        // OnClickListener interface for the loginButton.
+        View.OnClickListener loginButtonListener = new View.OnClickListener() {
+
+            final String full_name = editTextFullName.getText().toString();
+            final String user_name = editTextFullName.getText().toString();
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
+                intent.putExtra("full_name", full_name);
+                intent.putExtra("user_name", user_name);
+                startActivity(intent);
+            }
+        };
+
+        // Setting up the clickListener to the loginButton.
+        loginButton.setOnClickListener(loginButtonListener);
+    }
+}
---
+++ app/src/main/java/com/satyajeet/todonotes/MyNotesActivity.java
@@ -0,0 +1,18 @@
+package com.satyajeet.todonotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MyNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_notes);
+
+        Intent intent = getIntent();
+        
+    }
+}
